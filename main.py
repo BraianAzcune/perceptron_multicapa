4967345fdb41ee84b3e1b243f98b5fa176773d18
BraianAzcune <braian.s.azcune@hotmail.com>
2020-11-11 01:41:01 +0000

    primer commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#cargar datos
+
+#configurar red neuronal o cargar la red neuronal previa
+
+
+#entrenamiento o prediccion
+
+
+#mostrar o guardar resultados
---
+++ persistencia_red_neuronal.py
@@ -0,0 +1,12 @@
+import dill as pickle
+
+def guardar(red_neuronal):
+    """Dada la red neuronal (un array de neural_layer) guarda todo (incluso las funciones de activacion)"""
+    with open('red_neuronal.pkl', 'wb') as output:
+        pickle.dump(red_neuronal, output, pickle.HIGHEST_PROTOCOL)
+
+def cargar():
+    """Carga la ultima red neuronal usada, si no hay ninguna error"""
+    with open('red_neuronal.pkl', 'rb') as input:
+        red = pickle.load(input)
+        return red
